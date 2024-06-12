bc175014e3e45f76a6cf394ce63cae47364b8ab0
chaitanya-kumar33 <chaitanyakumar1922@gmail.com>
2024-06-12 18:17:53 -0400

    adde docs and removed unused imports

--- tests/test_evaluations.py
+++ tests/test_evaluations.py
@@ -1,4 +1,7 @@
-'''My Calculator Test'''
+"""
+Test cases for the Evaluations class.
+"""
+
 from decimal import Decimal
 import pytest
 from calculator.evaluation import Evaluation
@@ -7,28 +10,39 @@ from calculator.operations import add, subtract
 
 @pytest.fixture
 def setup_calculations():
-    """Clear history and add sample calculations for tests."""
+    """
+    Fixture to setup calculations.
+    """
     Evaluations.clear_history()
     Evaluations.add_evaluation(Evaluation(Decimal('10'), Decimal('5'), add))
     Evaluations.add_evaluation(Evaluation(Decimal('20'), Decimal('3'), subtract))
 
 def test_add_calculation(setup_calculations):
-    """Test adding a calculation to the history."""
+    """
+    Test adding calculation to history.
+    """
     calc = Evaluation(Decimal('4'), Decimal('4'), add)
     Evaluations.add_evaluation(calc)
     assert Evaluations.get_latest() == calc, "Failed to add the calculation to the history"
 
 def test_get_history(setup_calculations):
-    """Test retrieving the entire calculation history."""
+    """
+    Test getting calculation history.
+    """
     history = Evaluations.get_history()
     assert len(history) == 2, "History does not contain the expected number of evaluations"
 
 def test_clear_history(setup_calculations):
-    """Test clearing the entire calculation history."""
+    """
+    Test clearing calculation history.
+    """
     Evaluations.clear_history()
     assert len(Evaluations.get_history()) == 0, "History was not cleared"
 
 def test_get_latest(setup_calculations):
-    """Test getting the latest calculation from the history."""
+    """
+    Test getting the latest calculation.
+    """
     latest = Evaluations.get_latest()
     assert latest.a
+    
